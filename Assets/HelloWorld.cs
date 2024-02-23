c6ad84d26546094dafcd30f46d78a913b8e24b2b
PatriciaRdrz <patricia.r@opendeusto.es>
2024-02-23 09:04:57 +0100

    namespace2

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -2,18 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloWorld : MonoBehaviour
+namespace Game123
 {
-    // Start is called before the first frame update
-    void Start()
+    public class HelloWorld : MonoBehaviour
     {
-        Enemy enemy = new Enemy(" Voldemort", 2, true);
-        Player player = new Player(" HP", 4, true);
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
+            Enemy enemy = new Enemy(" Voldemort", 2, true);
+            Player player = new Player(" HP", 4, true);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.Log("Hello World");
+        // Update is called once per frame
+        void Update()
+        {
+            Debug.Log("Hello World");
+        }
     }
 }
